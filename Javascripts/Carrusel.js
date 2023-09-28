2ea6fccaaa540e0c9b1bd1d00b2eeb909425fabc
Sebastian fonseca <118923775+SebasFonse@users.noreply.github.com>
2023-09-28 16:10:50 -0500

    Gateria

--- Javascripts/Carrusel.js
+++ Javascripts/Carrusel.js
@@ -1,27 +1,48 @@
-// Asegúrate de que este nuevo código JavaScript esté debajo del código anterior.
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
 
-const carousel3 = document.querySelector('#carousel3');
-const prevButton3 = document.querySelector('[data-target="carousel3"].prev');
-const nextButton3 = document.querySelector('[data-target="carousel3"].next');
-const images3 = document.querySelectorAll('#carousel3 img');
-let currentIndex3 = 0;
-const slideWidth3 = images3[0].clientWidth;
 
-function goToIndex3(index) {
-    currentIndex3 = index;
-    carousel3.style.transform = `translateX(-${currentIndex3 * slideWidth3}px)`;
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() => {
+    moveToRight()
+}, 2000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
 }
-
-function nextSlide3() {
-    currentIndex3 = (currentIndex3 + 1) % images3.length;
-    goToIndex3(currentIndex3);
-}
-
-function prevSlide3() {
-    currentIndex3 = (currentIndex3 - 1 + images3.length) % images3.length;
-    goToIndex3(currentIndex3);
-}
-
-nextButton3.addEventListener('click', nextSlide3);
-prevButton3.addEventListener('click', prevSlide3);
-setInterval(nextSlide3, 2000); // Cambiar de imagen cada 2 segundos (ajusta según tus preferencias)
