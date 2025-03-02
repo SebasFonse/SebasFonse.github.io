e4492609d1c77e08678d32e4614c89dab9a875aa
Sebastian fonseca <118923775+SebasFonse@users.noreply.github.com>
2025-03-02 18:29:13 -0500

    subir

--- js/sripy.js
+++ js/sripy.js
@@ -1,3 +1,13 @@
-onload = () => {
-    document.body.classList.remove("container");
-  };
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".tarjeta").forEach(tarjeta => {
+      tarjeta.addEventListener("mouseenter", function () {
+          this.querySelector(".frente").style.transform = "rotateY(180deg)";
+          this.querySelector(".atras").style.transform = "rotateY(0)";
+      });
+
+      tarjeta.addEventListener("mouseleave", function () {
+          this.querySelector(".frente").style.transform = "rotateY(0)";
+          this.querySelector(".atras").style.transform = "rotateY(180deg)";
+      });
+  });
+});
